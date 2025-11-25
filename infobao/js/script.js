9117b4c81dba3a9e97e124754c90c1d07292aa75
Nazarius23 <147510440+Nazarius23@users.noreply.github.com>
2025-11-25 09:58:48 +0200

    Add files via upload

---
+++ infobao/js/script.js
@@ -0,0 +1,21 @@
+'use strict'
+let documentAction = e => {
+	const targetElement = e.target
+	if (targetElement.closest('.icon-menu')) {
+		document.documentElement.classList.toggle('open-menu')
+	}
+}
+document.addEventListener('click', documentAction)
+
+document.addEventListener('DOMContentLoaded', () => {
+	const header = document.querySelector('.header')
+	if (!header) return
+
+	window.addEventListener('scroll', () => {
+		if (window.scrollY > window.innerHeight) {
+			header.classList.add('header--bg')
+		} else {
+			header.classList.remove('header--bg')
+		}
+	})
+})
