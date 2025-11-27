3134796759929e5188d67ff8d11058451c1184a8
Nazarius23 <147510440+Nazarius23@users.noreply.github.com>
2025-11-27 20:38:06 +0200

    Add files via upload

---
+++ hedphone/js/script.js
@@ -0,0 +1,31 @@
+/*====================================================================================*/
+/*====================================================================================*/
+
+/* Меню бургер========================================================================*/
+
+// 'use strict'
+// let documentAction = e => {
+// 	const targetElement = e.target
+// 	if (targetElement.closest('.icon-menu')) {
+// 		document.documentElement.classList.toggle('open-menu')
+// 	}
+// }
+// document.addEventListener('click', documentAction)
+
+/*====================================================================================*/
+/*====================================================================================*/
+
+/* Появлення БГ в ховера при пертені якогось контейнера================================*/
+
+// document.addEventListener('DOMContentLoaded', () => {
+// 	const header = document.querySelector('.header')
+// 	if (!header) return
+
+// 	window.addEventListener('scroll', () => {
+// 		if (window.scrollY > window.innerHeight) {
+// 			header.classList.add('header--bg')
+// 		} else {
+// 			header.classList.remove('header--bg')
+// 		}
+// 	})
+// })
